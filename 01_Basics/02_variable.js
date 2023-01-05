daac6902bc429a9c32a6a598d2c3dce11f5ca064
ashish-ash303 <ashishdotc4@gami.com>
2023-01-05 20:54:57 +0530

    First 3 Tutorial Done

---
+++ 01_Basics/02_variable.js
@@ -0,0 +1,7 @@
+const name="Ashish"; 
+let name1="Ashish";
+console.log(name1);
+let number;
+console.table([name,name1,number])
+
+// Var cannot be used because of issue in block and function scope 
---
+++ 01_Basics/03_Datatype.js
@@ -0,0 +1,7 @@
+// output of null is object while output of undefined is  undefined 
+// typeof is used to check the type of a variabe
+
+let Check =44
+
+console.log(typeof Check)
+console.log(typeof (Check))
