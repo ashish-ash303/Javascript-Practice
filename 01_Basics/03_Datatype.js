0693051374836614f90eb72e4f0aea31543faebf
ashish-ash303 <ashishdotc4@gami.com>
2023-01-05 21:35:48 +0530

    conversion of datatype

--- 01_Basics/03_Datatype.js
+++ 01_Basics/03_Datatype.js
@@ -1,7 +1,27 @@
 // output of null is object while output of undefined is  undefined 
 // typeof is used to check the type of a variabe
 
-let Check =44
+let Check ="44xy"
+let value=44
 
-console.log(typeof Check)
-console.log(typeof (Check))
+//console.log(typeof Check)
+//console.log(typeof (Check))
+
+let value_converter=Number(Check);
+//console.log(value_converter)
+//console.log(value)
+
+
+let number=22
+let stringconversion=String(number);
+
+
+console.log(stringconversion)
+console.log(typeof stringconversion)
+
+// Number =>Number
+// Boolean value=>1 or 0
+// "44xy"=>NaN
+// 1 => True or string
+// "" => False in boolean case
+// String =>Number
